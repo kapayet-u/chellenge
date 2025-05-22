535386521950076d654e034171acb483d889952b
kapayet-u <kapayetu881@gmail.com>
2025-05-23 03:12:19 +0600

    Add files via upload

---
+++ say hello with.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+int main()
+{
+    string S="Hello, ";
+    string Input;
+    cin >> Input;
+
+    cout << S << Input << '\n';
+
+    return 0;
+  
+
+}
