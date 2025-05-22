1ae9cb3808d2604f68f097e3fb6620e16113f04f
kapayet-u <kapayetu881@gmail.com>
2025-05-22 23:33:01 +0600

    Add files via upload

---
+++ aria of circle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main ()
+{
+    double R;
+    cin >> R;
+    const double PI = 3.141592653;
+    double area = PI * R * R;
+
+    cout << fixed << setprecision(9) << area << endl;
+
+    return 0;
+
+
+
+}
