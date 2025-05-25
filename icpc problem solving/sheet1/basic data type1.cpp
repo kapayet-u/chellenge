cdd0a1a45cb8e3181e1d84bf92dd75be0bcd13a8
kapayet-u <kapayetu881@gmail.com>
2025-05-26 03:37:25 +0600

    Add files via upload

---
+++ icpc problem solving/sheet1/basic data type1.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    long long int y;
+    char z;
+    float i;
+    double j;
+
+    cin >> x >> y >> z >> i >> j;
+
+    cout << x << '\n' << y << '\n' << z << '\n' << i << '\n' << j;
+
+
+}
