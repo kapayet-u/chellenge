c0616bbc7ef16772dd30042cc131f570125c1eeb
kapayet-u <kapayetu881@gmail.com>
2025-05-26 03:34:45 +0600

    Add files via upload

---
+++ icpc problem solving/Sheet2/1 to N.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    int N;
+    cin >> N;
+  
+    for (int i = 1; N >= i; i++)
+    {
+        cout << i <<'\n';
+    }
+    
+
+
+
+    return 0;
+}
