f7450a3834f03c6229eed0825152b8a0fc203ebb
kapayet-u <kapayetu881@gmail.com>
2025-05-28 02:23:30 +0600

    Add files via upload

---
+++ icpc problem solving/Sheet2/fixed password.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+ 
+    int pass;
+
+    while (cin>>pass)
+    {
+        if(pass==1999)
+    {
+        cout << "Correct"<<'\n';
+        break;
+    }
+    else
+    {
+        cout << "Wrong" <<'\n'; 
+    }
+    }
+    
+    
+
+
+    return 0;
+}
