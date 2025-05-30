fa5c03ab22400b321b2728ff651a0cd14a627620
kapayet-u <kapayetu881@gmail.com>
2025-05-30 18:16:02 +0600

    Add files via upload

---
+++ icpc problem solving/Sheet2/phiramid.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    int n;
+    cin >> n;
+     for(int i= 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+           
+        {
+            cout << "*" ;
+        }
+        cout << "\n";
+    }
+    
+    
+
+
+
+    return 0;
+}
