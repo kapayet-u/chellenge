d7fb83673b77ae2d23ba5f842721a495fa6839fe
Md Kapayet ullah nahian <kapayetu881@gmail.com>
2025-06-24 20:53:34 +0600

    Add files via upload

---
+++ icpc problem solving/sheet 04/creat a new string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    string s, t;
+    cin >> s >> t;
+     
+    cout << s.length() << " " << t.length() << '\n';
+    cout << s << " " << t << '\n';
+    
+    return 0;
+}
