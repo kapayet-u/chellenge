848756be664f2dfb9e6ad3a682924f99b611f813
Md Kapayet ullah nahian <kapayetu881@gmail.com>
2025-06-25 20:01:32 +0600

    Add files via upload

---
+++ icpc problem solving/sheet 04/compare.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    fast;
+    string x, y;
+    cin >> x >> y;
+    if (x < y)
+    {
+      cout << x << '\n';
+    }
+    else
+    {
+      cout << y << '\n';  
+    
+    }
+      
+    return 0;
+}
