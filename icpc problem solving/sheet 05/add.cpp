68aa552f1fbba396cb2117aa7dbd487176421ec7
Md Kapayet ullah nahian <kapayetu881@gmail.com>
2025-07-05 23:20:41 +0600

    Add files via upload

---
+++ icpc problem solving/sheet 05/add.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int sum (int a, int b)
+{
+    return a + b;
+}
+
+int main() {
+    fast;
+    int a, b;
+    cin >> a >> b;
+    int result = sum (a, b);
+    cout << result << "\n";
+
+    
+    return 0;
+}
